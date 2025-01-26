68c50bd149e0506a48168b6f0ca51acb55c90862
Joshua Wong <joshwonghc@gmail.com>
2025-01-26 13:40:45 +0800

    Browse through generic functions chapter 4

--- src/main/scala/shapelessfun/Main.scala
+++ src/main/scala/shapelessfun/Main.scala
@@ -28,28 +28,32 @@ object Main {
     implicit val hNilEncoder: CsvEncoder[HNil] = instance(hnil => Nil)
 
     implicit def hListEncoder[H, T <: HList](implicit
-        hEncoder: CsvEncoder[H],
+        // Using Lazy[] prevents divergence at compilation by deferring
+        // evaluation to runtime and allowing self-referential implicits
+        hEncoder: Lazy[
+          CsvEncoder[H]
+        ],
         tEncoder: CsvEncoder[T]
     ): CsvEncoder[H :: T] = instance { case h :: t =>
-      hEncoder.encode(h) ++ tEncoder.encode(t)
+      hEncoder.value.encode(h) ++ tEncoder.encode(t)
     }
 
     implicit def genericEncoder[A, R](implicit
-        gen: Generic.Aux[A, R],
+        gen: Lazy[Generic.Aux[A, R]],
         enc: CsvEncoder[R]
         // gen.to(a) converts the generic to a hList.
         // Resolution is done by our hList encoders above
-    ): CsvEncoder[A] = instance(a => enc.encode(gen.to(a)))
+    ): CsvEncoder[A] = instance(a => enc.encode(gen.value.to(a)))
 
     implicit val cNilEncoder: CsvEncoder[CNil] =
       instance(cnil => sys.error("Not possible"))
 
     implicit def coproductEncoder[H, T <: Coproduct](implicit
-        hEncoder: CsvEncoder[H],
+        hEncoder: Lazy[CsvEncoder[H]],
         tEncoder: CsvEncoder[T]
     ): CsvEncoder[H :+: T] = instance {
       // disjunction of types, hence the match statement
-      case Inl(h) => hEncoder.encode(h)
+      case Inl(h) => hEncoder.value.encode(h)
       case Inr(t) => tEncoder.encode(t)
     }
   }
@@ -71,12 +75,29 @@ object Main {
   def writeCsv[A](values: List[A])(implicit enc: CsvEncoder[A]): String =
     values.map(value => enc.encode(value).mkString(",")).mkString("\n")
 
+  object DependentFunctions {
+    trait Second[L <: HList] {
+      type Out
+      def apply(l: L): Out
+    }
+
+    object Second {
+      type Aux[L <: HList, O] = Second[L] { type Out = O }
+      // Convenient wrapper to avoid having to do Second[L] {type Out = O} like Generic.Aux
+      // If we put return type as Second[L] in apply, Out member type will be erased
+      // This also prevents the compiler from using the member type for further implicit resolution
+      def apply[L <: HList](implicit inst: Second[L]): Aux[L, inst.Out] = inst
+    }
+  }
+
   def main(array: Array[String]): Unit = {
     import CsvEncoder._
 
     implicit val intEncoder: CsvEncoder[Int] =
       instance(num => List(num.toString))
     implicit val strEncoder: CsvEncoder[String] = instance(s => List(s))
+
+    // import scala.reflect.runtime.universe.reify allows us to debug implicit resolution
 
     /** writeCsv(dogs)(
       *    genericEncoder(
